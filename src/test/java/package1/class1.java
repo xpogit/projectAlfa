1a035bba788eba22c4fcd21267dc995c37202892
xpogit <azuredevopsmun@gmail.com>
2020-04-28 19:51:40 -0500

    1st from branch1 commiting

--- src/test/java/package1/class1.java
+++ src/test/java/package1/class1.java
@@ -1,8 +1,11 @@
 package package1;
 
+import java.sql.SQLOutput;
+
 public class class1 {
 
     public static void main(String[] args) {
         System.out.println("Class 1");
+        System.out.println("Its been modified in Branch1");
     }
 }
